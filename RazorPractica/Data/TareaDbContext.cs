12475d4295b5b1fc2a87d388284eddbcf1f7d166
Lurey14 <noemy.nmg@gmail.com>
2025-08-26 21:01:10 -0400

    base-datos-conexion

---
+++ RazorPractica/Data/TareaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPractica.Models;
+
+namespace RazorPractica.Data
+{
+    public class TareaDbContext : DbContext
+    {
+        public TareaDbContext(DbContextOptions<TareaDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+        protected TareaDbContext()
+        {
+
+        }
+    }
+}
