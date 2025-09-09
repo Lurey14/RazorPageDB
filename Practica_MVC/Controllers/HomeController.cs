786b416487a72c96a1557c45d6d316039f0ff0ac
Lurey14 <noemy.nmg@gmail.com>
2025-09-09 19:35:40 -0400

    migracion

---
+++ Practica_MVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Practica_MVC.Models;
+
+namespace Practica_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel Mode = new HomeModel();
+            Mode.Mensaje = "Hola Mundito!";
+            //Mode.Destinatario = "Docentes";
+            return View(Mode);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practica_MVC/Models/HomeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Practica_MVC.Models
+{
+    public class HomeModel
+    {
+        public string? Mensaje { get; set; }
+        public string? Destinatario { get; set; }
+    }
+}
